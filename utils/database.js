306ae8a07b0f5bf1aeebb267ba9fc97c53b20d1d
Uttiya WinHome <uttiyaghosh@gmail.com>
2024-02-22 13:41:12 -0500

    Removed node modules

--- utils/database.js
+++ utils/database.js
@@ -2,15 +2,18 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const options = {
   serverSelectionTimeoutMS: 60000,
-  connectTimeoutMS: 30000, // Increase this value as needed
-  socketTimeoutMS: 30000, // Increase this value as needed
+  connectTimeoutMS: 30000,
+  socketTimeoutMS: 30000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  maxPoolSize: 10
 };
 
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.DB_NAME}`;
 
 async function connect() {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(uri,options);
     return mongoose;
  } catch (e) {
   console.error(e);
